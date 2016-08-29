dab5cc3613a0030537cd185b6a6c665935adb535
Tom Most <twm@freecog.net>
2016-08-28 19:00:19 -0700

    Add copyright notice

--- src/pointbuy.js
+++ src/pointbuy.js
@@ -8,6 +8,7 @@ const Page = React.createClass({
         return <div>
             <h1><img src={headerUrl} alt="Point Buy Calculator" width="500" height="55" /></h1>
             <Calculator />
+            <p><small><a href="https://github.com/twm/peanutbutter">Peanut Butter</a> ©{"\x1b"}2016 Tom Most.<br />Includes React ©{"\x1b"}2013–2016 Facebook, Inc.</small></p>
         </div>;
     },
 });
